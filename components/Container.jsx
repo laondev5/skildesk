cc40d9d2f01d3b26c5cded91af9d1eaf801dab68
laondev5 <ekohabraham5@gmail.com>
2024-02-15 14:17:11 -0800

    nav abd hero commit

---
+++ components/Container.jsx
@@ -0,0 +1,6 @@
+  const Container = ({ children }) => {
+    return <div className=" mx-auto max-w-7xl w-full">{children}</div>;
+  };
+  
+  export default Container;
+  
---
+++ components/Navber.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Container from "./Container";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { MenuIcon, Bell } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+
+
+const navItem = [
+    { name: "Home", link:"/home" },
+    { name: "About", link:"/about" },
+    { name: "Contact", link:"/contact" },
+];
+const Navber = () => {
+  return (
+    <header className="py-2 z-[10] sticky top-0 left-0 bg-white">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="ml-2 lg:ml-0">Logo</div>
+          {/* desktop navbar */}
+          <div className="hidden lg:flex">
+          {navItem.map((route, i) => (
+                  <Button key={i} asChild variant="ghost">
+                    <Link
+                      key={i}
+                      href={route.link}
+                      className="text-sm transition-colors font-medium"
+                    >
+                      {route.name}
+                    </Link>
+                  </Button>
+                ))}
+          </div>
+          {/* mobile navbar */}
+          <div className="flex lg:hidden">
+          <Sheet>
+              <SheetTrigger>
+                <MenuIcon className="w-6 h-6 lg:hidden mr-2 lg:mr-2" />
+              </SheetTrigger>
+              <SheetContent>
+                <nav className="flex flex-col mx-6 space-y-4 lg:space-x-6">
+                 
+                      {navItem.map((route, i) => (
+                        <Button key={i}  asChild variant="ghost">
+                          <Link
+                            key={i}
+                            href={route.link}
+                            className="text-sm transition-colors font-medium"
+                          >
+                            {route.name}
+                          </Link>
+                        </Button>
+                      ))}
+                   
+                  
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Navber;

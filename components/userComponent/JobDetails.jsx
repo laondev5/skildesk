d33d40894475615842b02657d00b5bd5a45fb31e
laondev5 <ekohabraham5@gmal.com>
2024-07-11 12:56:00 +0100

    s

--- components/userComponent/JobDetails.jsx
+++ components/userComponent/JobDetails.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 const JobDetail = ({jobs, jobId}) => {
     console.log(jobId, jobs)
 
-    const [job, setJob] = useState(null);
+    const [job, setJob] = useState({});
 
   useEffect(() => {
     // Find job by id

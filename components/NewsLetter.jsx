5f88eb856523b49f028194c1a16b1fe33b727ed9
laondev5 <ekohabraham5@gmail.com>
2024-07-18 02:55:11 +0100

    newsletter2

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -26,12 +26,12 @@ export default function NewsLetter() {
   return (
     <>
       {isOpen ? (
-        <div className="absolute top-0 left-0 bg-gray-500/5 w-full h-full z-50">
+        <div className="fixed top-0 left-0 bg-gray-500/5 w-full h-full z-50">
           <div className="flex justify-center items-center w-full h-[100%]">
             <div className="w-[55%] h-[30rem] relative  flex justify-center items-center">
               <div
                 onClick={() => setIsOpen(false)}
-                className="absolute top-[6rem] right-2  lg:top-[8rem] lg:right-[18rem]"
+                className="absolute top-[6rem] right-2  lg:top-[8rem] lg:right-[10rem]"
               >
                 <Button size="icon">
                   <X />

4052fbe4f9c7457da5d751b71fda6659449b59c7
laondev5 <ekohabraham5@gmail.com>
2024-08-26 15:04:14 +0100

    dashg

--- app/api/getJobs/route.js
+++ app/api/getJobs/route.js
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+// import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function GET() {
-  try {
-    const jobs = await prisma.Jobs.findMany();
-    console.log(jobs);
-    if (!jobs) {
-      console.log("No job found");
-    }
+// export async function GET() {
+//   try {
+//     const jobs = await prisma.Jobs.findMany();
+//     console.log(jobs);
+//     if (!jobs) {
+//       console.log("No job found");
+//     }
 
-    return jobs;
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     return jobs;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
